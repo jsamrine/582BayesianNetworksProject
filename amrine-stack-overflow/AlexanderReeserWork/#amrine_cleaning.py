ed9a1dc4bf1fed1a35ee8bbed00a03e5cbeef116
unknown <julian.amrine@gmail.com>
2018-04-13 18:56:48 -0400

    Added new cleaning parameters to cleaning script

--- amrine-stack-overflow/AlexanderReeserWork/#amrine_cleaning.py
+++ amrine-stack-overflow/AlexanderReeserWork/#amrine_cleaning.py
@@ -27,8 +27,8 @@ def try_parse_e(string):
 		tens = int(split_val[1])
 
 		ret_val = first_digit * (10**tens)
-		
-	
+
+
 	return ret_val
 
 
@@ -38,10 +38,17 @@ def try_parse_dec(string):
 	return ret_val
 
 outlist = []
+"""
 keys = ['Race', 'Gender', 'Country', 'MajorUndergrad', \
 		'DeveloperType', 'CompetePeers', 'CompanySize', 'CompanyType', 'JobSatisfaction',\
 		'Salary', 'ExpectedSalary']
-language_keys = set()
+"""
+keys = ['MajorUndergrad', 'DeveloperType', 'CompetePeers', 'CompanySize', 'CompanyType', 'JobSatisfaction',\
+		'Salary']
+language_keys = set(["Objective-C", "PHP", "Perl", "Python", "R", "Ruby", "SQL",\
+"Swift", "TypeScript", "VB.NET", "VBA", "Assembly", "C", "C#", "C++", "Java", "JavaScript", "Other"])
+language_keys2 = set()
+
 min_salary = 10**100
 max_salary = 0
 gender_keys = ['male', 'female', 'nonbinary']
@@ -62,21 +69,20 @@ with open('survey_results_public.csv', 'r') as infile:
 
 			if "e" in row["Salary"]:
 				row["Salary"] = try_parse_e(row["Salary"])
-				# 
+				#
 			elif "." in row["Salary"]:
 				row["Salary"] = try_parse_dec(row["Salary"])
-			
+
 			min_salary = min(int(float(row["Salary"])), min_salary)
-			max_salary = max(int(float(row["Salary"])), max_salary)		
+			max_salary = max(int(float(row["Salary"])), max_salary)
 		if row["ExpectedSalary"] != "NA":
 			ExpectedSalaryCount += 1
 			if "e" in row["ExpectedSalary"]:
 				row["ExpectedSalary"] = try_parse_e(row["ExpectedSalary"])
-				
+
 			elif "." in row["ExpectedSalary"]:
 				row["ExpectedSalary"] = try_parse_dec(row["ExpectedSalary"])
 
-			
 			min_salary = min(int(float(row["ExpectedSalary"])), min_salary)
 			max_salary = max(int(float(row["ExpectedSalary"])), max_salary)
 		for gender in row["Gender"].split(";"):
@@ -89,24 +95,25 @@ with open('survey_results_public.csv', 'r') as infile:
 			else:
 				row["Gender"] = "Nonbinary"
 			break
-		
+
 		if row["HaveWorkedLanguage"] != "NA":
 			split_languages = row["HaveWorkedLanguage"].replace(" ", "").replace(",","").split(";")
 			for language in split_languages:
-				language_keys.add("Have%s"%language)
-				row["Have%s"%language] = 1
+				if language in language_keys:
+					language_keys2.add("Have%s"%language)
+					row["Have%s"%language] = 1
 		if row["WantWorkLanguage"] != "NA":
 			split_languages = row["WantWorkLanguage"].replace(" ", "").replace(",","").split(";")
 			for language in split_languages:
-				language_keys.add("Want%s"%language)
-				row["Want%s"%language] = 1
+				if language in language_keys:
+					language_keys2.add("Want%s"%language)
+					row["Want%s"%language] = 1
 		for key in row.keys():
 			if row[key] == "NA" or row[key] == "I don't know" or row[key] == "I prefer not to say":
 				row[key] = ""
-				
-		
+
 		outlist.append(row)
-for key in language_keys:
+for key in language_keys2:
 	keys.append(key)
 
 salary_brackets = [x*10000 for x in range(min_salary//10000, (max_salary//10000))]
@@ -125,7 +132,7 @@ for i in range(len(salary_brackets)-1):
 # print(SalaryCount)
 sorted_keys = sorted(keys)
 outlist_len = len(outlist)
-with open('amrine_cleaned_survey_resultsv1.1.csv', 'w') as outfile:
+with open('amrine_cleaned_survey_resultsv1.2.csv', 'w') as outfile:
 	outfile.write(" id ," + ",".join(sorted(keys)) + "\n")
 	for i in range(outlist_len):
 		writeRow = '%i' %i
@@ -133,15 +140,17 @@ with open('amrine_cleaned_survey_resultsv1.1.csv', 'w') as outfile:
 		for key in sorted_keys:
 			if (key == "Salary" or key == "ExpectedSalary") and outlist[i][key] != "":
 				index_of_bracket = bin_search(salary_brackets, int(outlist[i][key]), 0, len(salary_brackets))
-				print("Row: %i" %i)
-				print("Salary %i with bracket %i is less then %i" %(int(outlist[i][key]), index_of_bracket, salary_brackets[index_of_bracket]))
-				print("Salary %i goes in bracket: %s" %(int(outlist[i][key]), salary_brackets_str[index_of_bracket-1]))
-
 				writeRow = ",".join([writeRow, salary_brackets_str[index_of_bracket-1]])
 			else:
 				try:
 					writeRow = ",".join([writeRow, str(outlist[i][key]).replace(",", "")])
 				except:
-					writeRow = ",".join([writeRow, "0"])
-		writeRow = "\n".join([writeRow, ''])
-		outfile.write(writeRow)
+					if outlist[i]["HaveWorkedLanguage"] == "" and "Have" in key:
+						writeRow = ",".join([writeRow, ""])
+					elif outlist[i]["WantWorkLanguage"] == "" and "Want" in key:
+						writeRow = ",".join([writeRow, ""])
+					else:
+						writeRow = ",".join([writeRow, "0"])
+		if ",," not in writeRow:
+			writeRow = "\n".join([writeRow, ''])
+			outfile.write(writeRow)
